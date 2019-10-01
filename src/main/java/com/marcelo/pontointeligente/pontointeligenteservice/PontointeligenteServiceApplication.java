a38c22e74cf2e0b69af3c39229181c832d2bab3d
Costa <marcelo.santos.costa@accenture.com>
2019-10-01 10:24:58 -0300

    base estrutura do projeto

---
+++ src/main/java/com/marcelo/pontointeligente/pontointeligenteservice/PontointeligenteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.pontointeligente.pontointeligenteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo/pontointeligente/pontointeligenteservice/PontointeligenteServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marcelo.pontointeligente.pontointeligenteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontointeligenteServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
